8004fc9f5a7efad58a075cc24d0e7bae91574d3e
m4ttmo <m4ttmo@gmail.com>
2024-05-07 22:46:36 +0100

    pushing

--- public/CinemaTycoon/CHscript.js
+++ public/CinemaTycoon/CHscript.js
@@ -3,10 +3,10 @@ var screenMod = document.getElementById("ScreenSetModal");
 var btn = document.getElementsByClassName("movieOption");
 var span = document.getElementsByClassName("close")[0];
 var MovID = [
-  { id: "tt1517268", cost: 500000 },
-  { id: "tt15398776", cost: 500000 },
-  { id: "tt10160976", cost: 500000 },
-  { id: "tt13957560", cost: 500000 },
+  { id: "tt1517268", cost: 100000 },
+  { id: "tt15398776", cost: 200000 },
+  { id: "tt10160976", cost: 300000 },
+  { id: "tt13957560", cost: 400000 },
   { id: "tt3291150", cost: 500000 },
 ];
 var Mov = [];
@@ -14,13 +14,15 @@ var scrNum;
 var scrOpt = [];
 var MyCinLineUp = [];
 var budget = 1000000;
+var Filmcost;
 var displayBudget = document.getElementById("budget");
+var select = document.getElementById("selectNumber");
 
 window.onload = function FilmLoad() {
   displayBudget.innerHTML = budget;
   for (let i = 0; i <= MovID.length; i++) {
     console.log(i);
-    //console.log(MovID[i].id);
+    console.log(MovID[i].cost);
     fetch("https://www.omdbapi.com/?i=" + MovID[i].id + "&apikey=991caacb")
       .then((response) => response.json())
       .then((data) => {
@@ -47,8 +49,17 @@ function MovieModal(i) {
   document.getElementById("cast").innerHTML = cast;
 }
 function PickScreen(x) {
-  screenMod.style.display = "block";
-  movieSelect(x);
+  if (typeof scrOpt[x] === "undefined") {
+    screenMod.style.display = "block";
+    movieSelect(x);
+  } else {
+    scrNum = x;
+    document.getElementById("Scimg" + scrNum).src = "";
+    budget = budget + Filmcost;
+    scrOpt.splice(x, 1);
+    displayBudget.innerHTML = budget;
+    console.log(scrOpt + " " + x);
+  }
 }
 span.onclick = function () {
   modal.style.display = "none";
@@ -62,8 +73,6 @@ window.onclick = function (event) {
   }
 };
 
-var select = document.getElementById("selectNumber");
-
 function movieSelect(x) {
   select.innerHTML = "";
   for (var i = 0; i < Mov.length; i++) {
@@ -73,12 +82,24 @@ function movieSelect(x) {
   scrNum = x;
 }
 function scrPick() {
-  scrOpt[scrNum] = select.value;
-  var index = Mov.map(function (e) {
-    return e.imdbID;
-  }).indexOf(scrOpt[scrNum]);
-  let poster = Mov[index].Poster;
-  document.getElementById("Scimg" + scrNum).src = poster;
-  screenMod.style.display = "none";
-  console.log(scrOpt);
+  if (budget <= 0) {
+    alert("your broke");
+  } else {
+    console.log("here");
+    scrOpt[scrNum] = select.value;
+    var index = Mov.map(function (e) {
+      return e.imdbID;
+    }).indexOf(scrOpt[scrNum]);
+    let poster = Mov[index].Poster;
+    document.getElementById("Scimg" + scrNum).src = poster;
+    screenMod.style.display = "none";
+    console.log(scrOpt);
+    var indexCost = MovID.map(function (e) {
+      return e.cost;
+    }).indexOf(scrOpt[scrNum]);
+    Filmcost = MovID[index].cost;
+    console.log(Filmcost);
+    budget = budget - Filmcost;
+    displayBudget.innerHTML = budget;
+  }
 }
